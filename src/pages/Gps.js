9e57f063c29a9420a1b0b3b5350de54b2947fa94
gabe <gabestangel70@gmail.com>
2021-01-18 17:27:02 -0600

    cleaning up code

--- src/pages/Gps.js
+++ src/pages/Gps.js
@@ -9,7 +9,6 @@ function Gps() {
         <GridLayout className="layout" cols={12} rowHeight={40} width={1900}>
             <div key="coordinatesWidget" data-grid={{x: 0, y: 0, w: 5, h: 6, minW: 6, minH: 4}}><Coordinates/></div>
             <div key="elevationWidget" data-grid={{x: 0, y: 0, w: 5, h: 6, minW: 6, minH: 4}}><Elevation/></div>
-
         </GridLayout>
         </div>
     );
