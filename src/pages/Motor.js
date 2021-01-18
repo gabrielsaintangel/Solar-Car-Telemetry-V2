a8ea3d8785a6294dd9e3ea6d4a050566af7eb06e
gabe <gabestangel70@gmail.com>
2021-01-18 17:26:23 -0600

    cleaning up code

--- src/pages/Motor.js
+++ src/pages/Motor.js
@@ -11,7 +11,6 @@ function Motor() {
             <div key="rpmWidget" data-grid={{x: 0, y: 0, w: 5, h: 6, minW: 6, minH: 4}}><Rpm /></div>
             <div key="speedWidget" data-grid={{x: 0, y: 10, w: 5, h: 6, minW: 3, minH: 3}}><Speed /></div>
             <div key="regenWidget" data-grid={{x: 5, y: 0, w: 5, h: 8, minW: 3, minH: 3}}><Regen /></div>
-
         </GridLayout>
         </div>
     );
